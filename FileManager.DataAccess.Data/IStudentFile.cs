6111a1d3c7a201a412d131bfb43e93970565a791
Raul <ruizvela271@gmail.com>
2020-03-01 11:44:54 +0100

    Added methods Add, Update y Delete to IStudentFile interface

--- FileManager.DataAccess.Data/IStudentFile.cs
+++ FileManager.DataAccess.Data/IStudentFile.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FileManager.Common.Layer;
 
 namespace FileManager.DataAccess.Data
 {
     interface IStudentFile
     {
+        void Add(Student student);
+        void Update(Student student);
+        void Delete(Student student);
     }
 }
