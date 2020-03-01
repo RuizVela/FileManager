e99da69220cdf072c9c5696406e3ad683dac1645
Raul <ruizvela271@gmail.com>
2020-03-01 11:45:45 +0100

    Created TextFile Class which implements IStudentFile interface

---
+++ FileManager.DataAccess.Data/TextFile.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FileManager.Common.Layer;
+
+namespace FileManager.DataAccess.Data
+{
+    class TextFile : IStudentFile
+    {
+        public void Add(Student student)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Student student)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Student student)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
